308431d3f29811bcc1d073c2095ed4e20b087d55
Derek Fountain <derek@scratters.com>
2024-01-23 12:50:07 +0000

    bunch of renaming to try to make it more obvious what's going on

--- rp2040_pio_restart.c
+++ rp2040_pio_restart.c
@@ -27,21 +27,88 @@ SOFTWARE.
 #include <stdint.h>
 #include <string.h>
 #include "pico/stdlib.h"
-#include "pico/stdlib.h"
 
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 
-#include "pio1.pio.h"
-#include "pio2.pio.h"
+#include "pio_first_test.pio.h"
+#include "pio_second_test.pio.h"
 
-const uint32_t PIO1_TEST_GP  = 15;
-const uint32_t PIO2_TEST_GP  = 16;
+const uint32_t PIO_FIRST_TEST_TEST_GP  = 15;
+const uint32_t PIO_SECOND_TEST_TEST_GP = 16;
 
-const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+const uint32_t LED_PIN                 = PICO_DEFAULT_LED_PIN;
 
+void main( void )
+{
+  gpio_init( PIO_FIRST_TEST_TEST_GP );
+  gpio_set_dir( PIO_FIRST_TEST_TEST_GP, GPIO_OUT );
+  gpio_put( PIO_FIRST_TEST_TEST_GP, 0 );
+
+  gpio_init( PIO_FIRST_TEST_TEST_GP );
+  gpio_set_dir( PIO_FIRST_TEST_TEST_GP, GPIO_OUT );
+  gpio_put( PIO_FIRST_TEST_TEST_GP, 0 );
+
+  gpio_init( LED_PIN );  gpio_set_dir( LED_PIN, GPIO_OUT );
+  gpio_put( LED_PIN, 1 );
+
+  PIO  test_pio  = pio0;
+
+  int32_t  first_test_sm        = pio_claim_unused_sm(test_pio, true);
+  uint32_t first_test_offset    = pio_add_program(test_pio, &pio_first_test_program);
+  pio_first_test_program_init(test_pio, first_test_sm, first_test_offset, PIO_FIRST_TEST_TEST_GP);
+
+  int32_t  second_test_sm       = pio_claim_unused_sm(test_pio, true);
+  uint32_t second_test_offset   = pio_add_program(test_pio, &pio_second_test_program);
+  pio_second_test_program_init(test_pio, second_test_sm, second_test_offset, PIO_SECOND_TEST_TEST_GP);
+
+  // Both PIO programs are loaded, both are disabled
+  // Both signal GPIOs are at 0
+
+//  while( true ) 
+//  {
+    // Configure first PIO program
+    // Raise signal GPIO
+    // Enable it
+    // Wait 5ms
+    // Disable it
+    // Lower signal GPIO
+    // Deconfigure it, unload it, etc (whatever)
+
+    // Wait 10ms
+
+    // Configure second PIO program
+    // Raise signal GPIO
+    // Enable it
+    // Wait 5ms
+    // Disable it
+    // Lower signal GPIO
+    // Deconfigure it (whatever)
+
+    // Wait 10ms
+
+    pio_sm_set_enabled(test_pio, first_test_sm, true);
+    test_pio->txf[first_test_sm] = (clock_get_hz(clk_sys) / (2 * 4)) - 3;
+    sleep_ms(10);
+//    pio_sm_set_enabled(test_pio, first_test_sm, false);
+
+    sleep_ms(5);
+
+    pio_sm_set_enabled(test_pio, second_test_sm, true);
+    test_pio->txf[second_test_sm] = (clock_get_hz(clk_sys) / (2 * 8)) - 3;
+    sleep_ms(10);
+//    pio_sm_set_enabled(test_pio, second_test_sm, false);
+
+
+    while(1){
+    sleep_ms(5);
+  }
+}
+
+
+#if 0
 void main( void )
 {
   gpio_init( PIO1_TEST_GP );  gpio_set_dir( PIO1_TEST_GP, GPIO_OUT );
@@ -76,3 +143,4 @@ void main( void )
     sleep_ms(1000);
   }
 }
+#endif
