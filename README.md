2991f038a1ce8b5dc08d762f22376aa32eca2530
Derek Fountain <derek@scratters.com>
2024-01-23 20:57:08 +0000

    finalise, update readme

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # rp2040-pio-restart
-Investigation into restarting PIO programs on the RP2040
+
+This is an investigation into loading, starting, stopping, restarting
+and unloading PIO programs on the RP2040.
+
+I was having a problem with a project I was working on. It used several
+PIO programs, each of which it loaded, ran, then unloaded, in turn,
+repeating indefinitely to test a piece of hardware. I was failing in
+intermittent and weird ways, so I write this code to use as a basis for
+working out what was happening.
+
+This code has 2 PIO program, both nicked from the Pico examples. One
+pulses a pin at a given frequency, the other pulses a pin in a squarewave.
+What these programs do isn't really important; what's important is that
+one is loaded, runs correctly, then is stopped and unloaded so the other
+program can be loaded and run - repeatedly.
+
+When running correctly the output looks like this:
+
+![alt text](images/final_test_signal.png "Test signal")
+
+Yellow is the low frequency PIO program, running for 50ms. That stops and
+the other test is put in place. The cyan trace is the high frequency square
+wave (it looks solid in that capture, but trust me, it's a wave). Purple is
+just showing (high) when either test is running.
+
+But honestly, you can ignore the above. What the tests do doesn't really
+matter. What's important is that one test starts, runs, then stops, then
+the other test starts, runs and stops, repeatedly. It wasn't clear from the
+RP2040 docs how to make that happen. The code is well commented and easy to
+fiddle about with.
+
+The answer to the original mystery is that when a PIO program is stopped and
+replaced by a different program, the state machine needs to be re-initialised.
+This is because the state machine has a program counter which isn't reset when
+the new program is loaded. So if you don't reset the program counter manually,
+when you start the state machine with the new program in place, it won't start
+at the first instruction, it will start at some arbitrary place. The fix is a
+call to pio_sm_init() (which sorts the PC out for you), or, as demonstrated
+here, manually doing the same steps as that call does.
+
+Well, it was useful to me, so it might be useful to someone else. :)
+
+Derek Fountain, January 2024
--- rp2040_pio_restart.c
+++ rp2040_pio_restart.c
@@ -23,6 +23,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+/*
+ * This is an exercise in setting up, then tearing down and restarting PIO
+ * programs on the RP2040. The objective is to set up and set running a PIO
+ * program (called "first test"), let it run for a while, then tear it down,
+ * reclaim all resources, and then set up and run another program (called
+ * "second test"). Repeat indefinitely.
+ *
+ * I was struggling to get a project working which used a battery of PIO based
+ * tests. It was behaving very oddly, and I tracked the issue down using this
+ * test code. (The issue was the lack of the injected "JMP 0" when the new
+ * program was loaded - see below.)
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -33,65 +46,105 @@ SOFTWARE.
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 
+/*
+ * The 2 PIO programs. It doesn't really matter what they are for this example.
+ * As it happens, I've taken the pio/blink example from the Pico development
+ * archive and called that "first test", and the pio/squarewave example and
+ * called that "second test". They're set up so I can see their outputs on
+ * the scope.
+ */
 #include "pio_first_test.pio.h"
 #include "pio_second_test.pio.h"
 
-const uint32_t STATUS_GP               = 14;
-
+/* Outputs - scope these pins */
 const uint32_t PIO_FIRST_TEST_TEST_GP  = 15;
 const uint32_t PIO_SECOND_TEST_TEST_GP = 16;
 
-/*  16Hz, period is 62.5ms. Measured at 31.3ms with a high signal, then 31.3ms with a low signal (62.6ms) */
-/* 100Hz, period is 10.0ms. Measured at  5.0ms with a high signal, then  5.0ms with a low signal (10.0ms) */
-/* 200Hz, period is  5.0ms. Measured at  2.5ms with a high signal, then  2.5ms with a low signal ( 5.0ms) */
+/* A third GPIO, toggled to indicate when a test is running */
+const uint32_t STATUS_GP               = 14;
+
+/* 200Hz, period is 5.0ms. Measured at 2.5ms with a high signal, then 2.5ms with a low signal (exactly 5.0ms) */
 const uint32_t FIRST_TEST_FREQ         = 200;
 
 const uint32_t LED_PIN                 = PICO_DEFAULT_LED_PIN;
 
 void main( void )
 {
+  /* LED shows we're running */
   gpio_init( LED_PIN );  gpio_set_dir( LED_PIN, GPIO_OUT );
   gpio_put( LED_PIN, 1 );
 
+  /* Configure status indicator GPIO */
   gpio_init( STATUS_GP );  gpio_set_dir( STATUS_GP, GPIO_OUT );
   gpio_put( STATUS_GP, 0 );
 
-  PIO      test_pio             = pio0;
-
-
-  // Both PIO programs are loaded, both are disabled
-  // Both signal GPIOs are at 0
-
   while( true ) 
   {
+    PIO test_pio = pio0;
+
     gpio_put( STATUS_GP, 1 );
 
+    /*
+     * Claim a state machine, add the first test program and initialise it.
+     * The API call pio_sm_init() does all of this, I think. Rather than use that,
+     * I'm spelling it all out just to be clear
+     */
     int32_t  first_test_sm        = pio_claim_unused_sm(test_pio, true);
     uint32_t first_test_offset    = pio_add_program(test_pio, &pio_first_test_program);
     pio_first_test_program_init(test_pio, first_test_sm, first_test_offset, PIO_FIRST_TEST_TEST_GP);
 
+    /*
+     * According to this post:
+     *  https://forums.raspberrypi.com/viewtopic.php?t=341316#p2045387
+     * it can be necessary to reset the PIO's program counter. Simply resetting and
+     * restarting the state machine doesn't do a JMP 0 (or wherever the PIO program
+     * starts) so if a state machine is stopped and a new program loaded into it,
+     * that program will start at an arbitrary location in the PIO code.
+     */
+    pio_sm_exec(test_pio, first_test_sm, pio_encode_jmp(first_test_offset));
+
+    /* This test needs to be given a frequency, so set it running and give it the value */
     pio_sm_set_enabled(test_pio, first_test_sm, true);
     test_pio->txf[first_test_sm] = (clock_get_hz(clk_sys) / (2 * FIRST_TEST_FREQ)) - 3;
+
+    /* Let it run a while */
     sleep_ms(50);
+
+    /* Tear down: disable it, clear the fifos, unclaim the state machine and remove the program */
     pio_sm_set_enabled(test_pio, first_test_sm, false);
     pio_sm_clear_fifos(test_pio, first_test_sm);
     pio_sm_unclaim(test_pio, first_test_sm);
     pio_remove_program(test_pio, &pio_first_test_program, first_test_offset);
 
+    /* Not sure about this, the init code does it. Unneeded for this example, might be for something else */
+    pio_sm_clkdiv_restart(test_pio, first_test_sm);
+
+
+
+    /* Another pause so the scope can show the gap */
     gpio_put( STATUS_GP, 0 );
     sleep_ms(20);
     gpio_put( STATUS_GP, 1 );
 
+
+
+    /* Same again - claim the state machine, load and initialise the second test program */
     int32_t  second_test_sm       = pio_claim_unused_sm(test_pio, true);
     uint32_t second_test_offset   = pio_add_program(test_pio, &pio_second_test_program);
     pio_second_test_program_init(test_pio, second_test_sm, second_test_offset, PIO_SECOND_TEST_TEST_GP);
 
+    pio_sm_exec(test_pio, second_test_sm, pio_encode_jmp(second_test_offset));
+
+    /* No need to poke this test program, just start it and let it run */
     pio_sm_set_enabled(test_pio, second_test_sm, true);
     sleep_ms(50);
+
+    /* Tear down and clean up as before */
     pio_sm_set_enabled(test_pio, second_test_sm, false);
     pio_sm_clear_fifos(test_pio, second_test_sm);
-    pio_sm_unclaim(test_pio, first_test_sm);
+    pio_sm_unclaim(test_pio, second_test_sm);
     pio_remove_program(test_pio, &pio_second_test_program, second_test_offset);
+    pio_sm_clkdiv_restart(test_pio, second_test_sm);
 
     gpio_put( STATUS_GP, 0 );
     sleep_ms(20);
